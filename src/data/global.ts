10beb036bec8095fff16ae7896c2e53f77366346
DeveloperRyou <DeveloperRyou@gmail.com>
2024-03-07 19:32:34 +0900

    FIX: AuthAtom 추가

--- src/data/global.ts
+++ src/data/global.ts
@@ -3,5 +3,14 @@ import { atom } from "jotai";
 
 const myAtom = atom<My | null>(null);
 const storeIdAtom = atom<string | null>(null);
+const authAtom = atom<"owner" | "manager" | "staff">(get => {
+  const my = get(myAtom);
+  my?.relationList.forEach(relation => {
+    if (relation.storeId === get(storeIdAtom)) {
+      return relation.role;
+    }
+  });
+  return "staff";
+});
 
-export { myAtom, storeIdAtom };
+export { myAtom, storeIdAtom, authAtom };
--- src/pages/manage/index.tsx
+++ src/pages/manage/index.tsx
@@ -1,32 +1,29 @@
-import StaffInform from "@/screen/manage/StaffInform";
+import { authAtom } from "@/data/global";
 import Schedule from "@/screen/manage/Schedule";
+import StaffInform from "@/screen/manage/StaffInform";
 import TabBarGage from "@modules/components/bars/TabBarGage";
+import FloatingActionButton from "@modules/components/button/FloatingActionButton";
 import LongTab from "@modules/components/tabs/LongTab";
 import FlexBox from "@modules/layout/FlexBox";
-import { useState, useEffect } from "react";
-import FloatingActionButton from "@modules/components/button/FloatingActionButton";
 import Icon from "@modules/layout/Icon";
+import { useAtom } from "jotai";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Manage() {
   const [selectTab, setSelectTab] = useState("left");
-  const [authority, setAutority] = useState("staff");
-
-  useEffect(() => {
-    // 권한 가져오기
-    setAutority("ceo"); // 임시 권한
-  }, []);
+  const [auth] = useAtom(authAtom);
 
   const router = useRouter();
   return (
     <FlexBox direction="col" className="h-full justify-between">
-      <FlexBox direction="col" className="w-full h-full gap-4">
+      <FlexBox direction="col" className="w-full h-full gap-4 py-4">
         <div className="B1-medium mt-1.5">직원 관리</div>
         <LongTab
           text={["근무 스케쥴", "직원 정보"]}
           pageHandle={Options => setSelectTab(Options)}
         />
-        {selectTab === "right" && authority === "ceo" && (
+        {selectTab === "right" && auth === "ceo" && (
           <div className="w-full px-4">
             <FlexBox
               direction="row"
@@ -50,10 +47,10 @@ export default function Manage() {
         {selectTab === "left" ? <Schedule /> : <StaffInform />}
       </FlexBox>
       <div className="ml-auto my-6 mx-4">
-        {selectTab === "left" && authority === "ceo" && (
+        {selectTab === "left" && auth === "owner" && (
           <FloatingActionButton text="근무 추가" />
         )}
-        {selectTab === "right" && authority === "ceo" && (
+        {selectTab === "right" && auth === "owner" && (
           <FloatingActionButton text="직원 추가" />
         )}
       </div>
