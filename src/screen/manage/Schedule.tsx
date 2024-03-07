072fbd6eb2368d2e7fbeae426039f2d44a07d61a
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-03-08 01:36:05 +0900

    FIX: Schedule.tsx 컬러 오류 수정

--- src/screen/manage/Schedule.tsx
+++ src/screen/manage/Schedule.tsx
@@ -20,29 +20,27 @@ export default function Schedule() {
       <FlexBox direction="col" className="w-full px-4 gap-3">
         <FlexBox className="w-full justify-between">
           <FlexBox className="gap-1">
-            <div className="H4-bold">{dayjs().format("DD")}</div>
-            <div className="B3-medium text-neutral-500">
-              {dayjs().format("ddd")}
-            </div>
+            <div className="H4-bold text-Gray7">{dayjs().format("DD")}</div>
+            <div className="B3-medium text-Gray5">{dayjs().format("ddd")}</div>
           </FlexBox>
           <FlexBox className="gap-4">
             <FlexBox className="gap-1">
-              <div className="w-3 h-4 bg-red-600 rounded" />
-              <div className="B5-medium text-neutral-600">매니저</div>
+              <div className="w-3 h-4 bg-Manager rounded" />
+              <div className="B5-medium text-Gray6">매니저</div>
             </FlexBox>
             <FlexBox className="gap-1">
-              <div className="w-3 h-4 bg-orange-600 rounded" />
-              <div className="B5-medium text-neutral-600">알바</div>
+              <div className="w-3 h-4 bg-PartTime rounded" />
+              <div className="B5-medium text-Gray6">알바</div>
             </FlexBox>
             <FlexBox className="gap-1">
-              <div className="w-3 h-4 bg-black rounded" />
-              <div className="B5-medium text-neutral-600">기타</div>
+              <div className="w-3 h-4 bg-Black rounded" />
+              <div className="B5-medium text-Gray6">기타</div>
             </FlexBox>
           </FlexBox>
         </FlexBox>
         <FlexBox className="gap-1 w-full justify-start">
-          <div className="text-zinc-400 text-[10px]">00:00</div>
-          <div className="w-full h-px bg-gray-200" />
+          <div className="C3 text-Gray4">00:00</div>
+          <div className="w-full h-px bg-Gray2" />
         </FlexBox>
         <FlexBox direction="col" className="w-full gap-2">
           {schedules.map((plan, index) => (
