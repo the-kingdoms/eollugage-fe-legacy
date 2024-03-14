bbb035a0063bee64116e823622e3ef1eb8e94ef1
PangKiYeon <pdrkeamy@gmail.com>
2024-03-14 18:51:27 +0900

    REFACTOR : relation 배열을 딕셔너리로 수정

--- src/screen/manage/StaffInform.tsx
+++ src/screen/manage/StaffInform.tsx
@@ -7,31 +7,53 @@ import { Relation } from "@/apis/relation";
 
 export default function StaffInform() {
   const { relations } = useGetRelation();
-  const [relation, setRelation] = useState<Relation[]>([]);
+  const [relationDict, setRelationDict] = useState<Record<string, Relation[]>>(
+    {},
+  );
 
   useEffect(() => {
     if (relations && relations.length > 0) {
-      setRelation(relations);
+      const updatedDict = relations.reduce(
+        (dict: Record<string, Relation[]>, relates) => {
+          dict[relates.position]
+            ? dict[relates.position].push(relates)
+            : (dict[relates.position] = [relates]);
+          return dict;
+        },
+        {},
+      );
+      setRelationDict(updatedDict);
     } else {
-      setRelation([]);
+      setRelationDict({});
     }
   }, [relations]);
 
   return (
-    <FlexBox direction="col" className="w-full px-2 items-start gap-4 px-4">
-      <div className="B1-medium">매니저</div>
-      <FlexBox direction="col" className="gap-2 w-full">
-        {relation.map((relate, index) => (
-          <RouterWrapper key={index} routerdest="/manage/staff" type="profile">
-            <ProfileDiscription
-              name={"얼루가"}
-              position={relate.position}
-              time={"00:00 - 00:00"}
-            />
-          </RouterWrapper>
-        ))}{" "}
-      </FlexBox>
-      <div className="B1-medium">알바</div>
+    <FlexBox direction="col" className="w-full items-start gap-6">
+      {Object.entries(relationDict).map(([position, relates], index) => (
+        <FlexBox
+          key={index}
+          direction="col"
+          className="items-start w-full gap-4"
+        >
+          <div className="B1-medium">{position}</div>
+          <FlexBox direction="col" className="gap-2 w-full">
+            {relates.map((relate, index) => (
+              <RouterWrapper
+                key={index}
+                routerdest="/manage/staff"
+                type="profile"
+              >
+                <ProfileDiscription
+                  name={"얼루가"}
+                  position={relate.position}
+                  time={"00:00 - 00:00"}
+                />
+              </RouterWrapper>
+            ))}{" "}
+          </FlexBox>
+        </FlexBox>
+      ))}
     </FlexBox>
   );
 }
