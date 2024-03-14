7b4809685a561b475be8dfb2aae6f9dd8918e9b5
DeveloperRyou <DeveloperRyou@gmail.com>
2024-03-14 22:56:06 +0900

    FIX: relation slider 생성

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,5 +1,6 @@
 import AdminControlBanner from "@/assist/banner/AdminControlBanner";
 import { roleAtom } from "@/data/global";
+import { useGetRelation } from "@/hooks/query/relation";
 import RelationSlider from "@/screen/mypage/RelationSlider";
 import UserInfo from "@/screen/mypage/UserInfo";
 import WorkHistoryList from "@/screen/mypage/WorkHistoryList";
@@ -7,9 +8,20 @@ import WorkHour from "@/screen/mypage/WorkHour";
 import TabBarGage from "@modules/components/bars/TabBarGage";
 import FlexBox from "@modules/layout/FlexBox";
 import { useAtom } from "jotai";
+import { useEffect, useState } from "react";
+import { Relation } from "@/apis/relation";
 
 export default function Manage() {
   const [role] = useAtom(roleAtom);
+  const [currentRelation, setCurrentRelation] = useState<Relation>(
+    {} as Relation,
+  );
+  const { relations } = useGetRelation();
+
+  useEffect(() => {
+    if (relations) setCurrentRelation(relations[0] ?? {});
+  }, [relations]);
+
   return (
     <FlexBox direction="col" className="relative h-full justify-between">
       <FlexBox direction="col" className="w-full gap-6 py-4">
@@ -20,7 +32,12 @@ export default function Manage() {
             {role === "OWNER" && <AdminControlBanner />}
           </div>
           <div className="w-full">
-            {(role === "MANAGER" || role === "OWNER") && <RelationSlider />}
+            {(role === "MANAGER" || role === "OWNER") && (
+              <RelationSlider
+                relationList={relations ?? []}
+                currentRelation={currentRelation}
+              />
+            )}
           </div>
         </FlexBox>
         <WorkHistoryList />
--- src/screen/mypage/RelationSlider.tsx
+++ src/screen/mypage/RelationSlider.tsx
@@ -6,41 +6,36 @@ import Slider from "@modules/layout/Slider";
 interface RelationSliderProps {
   currentRelation: Relation;
   relationList: Relation[];
+  onClick?: (relation: Relation) => void;
 }
 
 export default function RelationSlider({
   currentRelation,
   relationList,
+  onClick,
 }: RelationSliderProps) {
   return (
     <Slider className="px-4 gap-2">
-      <button className="shrink-0" type="button" onClick={() => {}}>
-        <FlexBox direction="col" className="gap-1">
-          <Profile size="M1" name="hell" focus={false} />
-          <div className="w-full text-center B5-medium text-Gray5">hello</div>
-        </FlexBox>
-      </button>
-      <div className="shrink-0">
-        <Profile size="M1" name="hell" />
-      </div>
-      <div className="shrink-0">
-        <Profile size="M1" name="hell" />
-      </div>
-      <div className="shrink-0">
-        <Profile size="M1" name="hell" />
-      </div>
-      <div className="shrink-0">
-        <Profile size="M1" name="hell" />
-      </div>
-      <div className="shrink-0">
-        <Profile size="M1" name="hell" />
-      </div>
-      <div className="shrink-0">
-        <Profile size="M1" name="hell" />
-      </div>
-      <div className="shrink-0">
-        <Profile size="M1" name="hell" />
-      </div>
+      {relationList.map(relation => (
+        <button
+          key={relation.id}
+          className="shrink-0"
+          type="button"
+          onClick={() => onClick && onClick(relation)}
+        >
+          <FlexBox direction="col" className="gap-1">
+            <Profile
+              size="M1"
+              name={relation.id}
+              focus={relation.id === currentRelation.id}
+            />
+            <div className="w-full text-center B5-medium text-Gray5">
+              {relation.id.slice(0, 5)}
+              {relation.id.length > 5 && "..."}
+            </div>
+          </FlexBox>
+        </button>
+      ))}
     </Slider>
   );
 }
