12511ce7fc918bbd66c8f529b0b6f01427ac73b3
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-03-14 18:28:24 +0900

    FIX: getHistory Main페이지 이름, 직책 변수 연결

--- src/screen/main/TodayShift.tsx
+++ src/screen/main/TodayShift.tsx
@@ -1,17 +1,20 @@
+import { myAtom } from "@/data/global";
 import { useGetHistoryList } from "@/hooks/query/history";
 import WorkInfo from "@/screen/main/WorkInfo";
 import FlexBox from "@modules/layout/FlexBox";
+import { useAtom } from "jotai";
 
 export default function TodayShift() {
   const { data: historyList } = useGetHistoryList();
+  const [my] = useAtom(myAtom);
 
   return (
     <FlexBox direction="col" className="w-full gap-4 items-start">
       <div className="H5-bold">금일의 근무</div>
       {historyList?.map(historyInfo => (
         <WorkInfo
-          name="이름없음"
-          position="직책없음"
+          name={String(my?.name)}
+          position={String(my?.relationList[0].position)}
           time={`${historyInfo.startTime.slice(0, 5)} ~ ${historyInfo.endTime.slice(0, 5)}`}
         />
       ))}
