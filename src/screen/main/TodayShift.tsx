ab631bd5b53890152a8b8f25aeefa8369141a4bc
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-03-21 02:16:00 +0900

    FIX: Main페이지에 금일 근무만 보이게 수정

--- src/screen/main/TodayShift.tsx
+++ src/screen/main/TodayShift.tsx
@@ -2,17 +2,28 @@ import { myAtom, myMemberIdAtom } from "@/data/global";
 import { useGetHistoryList } from "@/hooks/query/history";
 import WorkInfo from "@/screen/main/WorkInfo";
 import FlexBox from "@modules/layout/FlexBox";
+import dayjs from "dayjs";
 import { useAtom } from "jotai";
+import { useEffect, useState } from "react";
 
 export default function TodayShift() {
   const [my] = useAtom(myAtom);
   const [myMemberId] = useAtom(myMemberIdAtom);
   const { data: historyList } = useGetHistoryList(myMemberId);
 
+  // 임시
+  const [filteredList, setFilteredList] = useState(historyList);
+  useEffect(() => {
+    const tempList = historyList?.filter(historyInfo => {
+      return historyInfo.date === dayjs().format("YYYY-MM-DD");
+    });
+    setFilteredList(tempList);
+  }, [historyList]);
+
   return (
     <FlexBox direction="col" className="w-full gap-4 items-start">
       <div className="H5-bold">금일의 근무</div>
-      {historyList?.map(historyInfo => (
+      {filteredList?.map(historyInfo => (
         <WorkInfo
           key={historyInfo.id}
           name={String(my?.name)} // 임시
