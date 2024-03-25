33ed04679feca0062b8bdd3c34303216dc4f8b0a
DeveloperRyou <DeveloperRyou@gmail.com>
2024-03-25 17:05:08 +0900

    FIX: popup 사이즈 변경

--- src/screen/manage/ShareLink.tsx
+++ src/screen/manage/ShareLink.tsx
@@ -51,29 +51,33 @@ function ShareLink() {
   }, []);
 
   return (
-    <FlexBox direction="col" className="w-full h-full px-4">
-      <div className="w-full mt-4">
-        <div className="H4-bold">링크를 직원에게</div>
-        <div className="H4-bold">공유해주세요</div>
-      </div>
-      <FlexBox direction="col" className="w-full h-full mt-8">
-        <Image
-          height={170}
-          width={170}
-          alt="link"
-          src="/image/share_link.png"
-        />
-        <div className="B1-medium text-Gray5 mt-4">링크복사가 안되었나요?</div>
-        <button onClick={handleCopyLink} type="button">
-          <div className="B2-regular text-Gray4">링크 복사하기</div>
-        </button>
+    <>
+      <FlexBox direction="col" className="w-full h-full px-4">
+        <div className="w-full mt-4">
+          <div className="H4-bold">링크를 직원에게</div>
+          <div className="H4-bold">공유해주세요</div>
+        </div>
+        <FlexBox direction="col" className="w-full h-full mt-8">
+          <Image
+            height={170}
+            width={170}
+            alt="link"
+            src="/image/share_link.png"
+          />
+          <div className="B1-medium text-Gray5 mt-4">
+            링크복사가 안되었나요?
+          </div>
+          <button onClick={handleCopyLink} type="button">
+            <div className="B2-regular text-Gray4">링크 복사하기</div>
+          </button>
+        </FlexBox>
       </FlexBox>
       {linkCopied && (
         <div className="w-full px-4 py-2 mb-4 bg-[#2D2D2D] text-white">
           링크가 자동으로 복사되었습니다.
         </div>
       )}
-    </FlexBox>
+    </>
   );
 }
 
