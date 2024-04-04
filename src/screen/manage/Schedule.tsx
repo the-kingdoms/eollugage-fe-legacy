f1ef93d2d62b3ed8cc3dbe952f96f8eec4f2f51f
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-04-04 12:41:25 +0900

    REFACTOR: ShowTimeLine 컴포넌트 분리

--- src/screen/manage/Schedule.tsx
+++ src/screen/manage/Schedule.tsx
@@ -1,30 +1,13 @@
 import { selectedDateAtom } from "@/data/historyAtom";
-import {
-  useDeleteHistory,
-  useGetAllMemberHistoryByDate,
-} from "@/hooks/query/history";
-import { getTimeString } from "@/libs/timeValidation";
+import { useGetAllMemberHistoryByDate } from "@/hooks/query/history";
 import Calender from "@modules/components/calender/Calender";
-import ScheduleList from "@modules/components/list/ScheduleList";
-import useDialog from "@modules/hooks/useDialog";
 import Divider from "@modules/layout/Divider";
 import FlexBox from "@modules/layout/FlexBox";
 import dayjs from "dayjs";
 import { useAtom } from "jotai";
+import ShowTimeLine from "./ShowTimeLine";
 
 export default function Schedule() {
-  const { mutate: deleteHistoryMutate } = useDeleteHistory();
-  const { openDialog } = useDialog();
-
-  const onClickDeleteBtn = (historyId: string) => {
-    openDialog({
-      title: "근무 삭제하기",
-      discription: "근무를 삭제하시나요?",
-      type: "confirm",
-      onAction: () => deleteHistoryMutate(historyId),
-    });
-  };
-
   const [selectedDate, setSelectedDate] = useAtom(selectedDateAtom);
   const onClickCalendar = (date: dayjs.Dayjs) => {
     setSelectedDate(date);
@@ -32,39 +15,6 @@ export default function Schedule() {
   const { data: historyList } = useGetAllMemberHistoryByDate(
     selectedDate.format("YYYY-MM-DD"),
   );
-
-  const showTimeLine = () => {
-    const time = historyList?.map(historyInfo => historyInfo.startTime);
-    const timeSet = new Set(time);
-    const timeLine = Array.from(timeSet);
-
-    return timeLine.map(time => (
-      <>
-        <FlexBox className="gap-1 w-full justify-start mb-3">
-          <div className="C3 text-Gray4">{time.slice(0, 5)}</div>
-          <div className="w-full h-px bg-Gray2" />
-        </FlexBox>
-        <FlexBox direction="col" className="w-full gap-2 mb-3">
-          {historyList?.map((historyInfo, index) => {
-            if (historyInfo.startTime == String(time)) {
-              return (
-                <ScheduleList
-                  key={index}
-                  name={historyInfo.relation.member.name}
-                  role={historyInfo.relation.role}
-                  time={getTimeString(
-                    historyInfo.startTime,
-                    historyInfo.endTime,
-                  )}
-                  onDelete={() => onClickDeleteBtn(historyInfo.id)}
-                />
-              );
-            }
-          })}
-        </FlexBox>
-      </>
-    ));
-  };
 
   return (
     <FlexBox direction="col" className="gap-3 w-full">
@@ -95,9 +45,7 @@ export default function Schedule() {
             </FlexBox>
           </FlexBox>
         </FlexBox>
-        <FlexBox direction="col" className="w-full">
-          {showTimeLine()}
-        </FlexBox>
+        <ShowTimeLine historyList={historyList} />
       </FlexBox>
     </FlexBox>
   );
