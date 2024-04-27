229818307a42d4deaa92237c83bbe522d6caf830
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-04-28 02:21:51 +0900

    FIX: 약관 페이지 border 오류 수정

--- src/pages/mypage/term.tsx
+++ src/pages/mypage/term.tsx
@@ -4,23 +4,26 @@ import TopTitle from "@modules/layout/TopTitle";
 
 export default function Term() {
   return (
-    <FlexBox direction="col" className="px-4 break-keep">
-      <TopTitle type="back" title="개인정보 처리방침" />
-      <div className="pt-8 pb-6 border-t border-Gray1">
+    <FlexBox direction="col" className="break-keep">
+      <div className="px-4 w-full">
+        <TopTitle type="back" title="개인정보 처리방침" />
+      </div>
+      <div className="h-[1px] w-full bg-Gray1" />
+      <div className="pt-8 pb-6 px-4">
         <div className="H5-bold mb-4">{termText.title}</div>
         <div className="text-Gray6 B4-regular !leading-5">
           {termText.description}
         </div>
-        {termText.parts.map(part => (
-          <FlexBox direction="col" className="items-start gap-2 mt-8">
+        {termText.parts.map((part, i) => (
+          <FlexBox key={i} direction="col" className="items-start gap-2 mt-8">
             <div className="text-Gray7 B3-medium mb-2 !leading-5">
               {part.title}
             </div>
             <div className="text-Gray6 B4-regular !leading-5">
               {part.description}
             </div>
-            {part.detail.map(detail => (
-              <li className="text-Gray6 B4-regular !leading-[22px]">
+            {part.detail.map((detail, index) => (
+              <li className="text-Gray6 B4-regular !leading-[22px]" key={index}>
                 {detail}
               </li>
             ))}
