e43b03e87a7e4198b6038dccbb774eb2646c92c3
DeveloperRyou <DeveloperRyou@gmail.com>
2024-05-19 18:22:57 +0900

    HOTFIX: KAKAO 로그인 버튼 동작하도록 수정

--- src/assist/buttonwrapper/KakaoLoginButtonWrapper.tsx
+++ src/assist/buttonwrapper/KakaoLoginButtonWrapper.tsx
@@ -1,23 +1,18 @@
 import LoginButton from "@modules/components/button/LoginButton";
 import FlexBox from "@modules/layout/FlexBox";
-import { useEffect } from "react";
 
 function KakaoLoginButtonWrapper() {
   const kakaoSDKLogin = () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const kakao = (window as any).Kakao;
+    if (!kakao?.isInitialized()) {
+      kakao?.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+    }
     const redirectUri = `https://api.eolluga.com/login/oauth2/code/kakao`;
     kakao?.Auth?.authorize({
       redirectUri,
     });
   };
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const kakao = (window as any)?.Kakao;
-    if (!kakao?.isInitialized()) {
-      kakao?.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
-    }
-  }, []);
   return (
     <FlexBox className="px-4 w-full justify-center">
       <div className="w-full max-w-[360px]">
