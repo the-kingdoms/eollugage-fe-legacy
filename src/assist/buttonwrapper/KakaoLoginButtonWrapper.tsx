fba4c0064fa39500655affeebaf66bb532417a7a
DeveloperRyou <DeveloperRyou@gmail.com>
2024-06-20 20:03:52 +0900

    FIX: kakao app login 실패시 REST login 제공

--- src/assist/buttonwrapper/KakaoLoginButtonWrapper.tsx
+++ src/assist/buttonwrapper/KakaoLoginButtonWrapper.tsx
@@ -1,24 +1,42 @@
 import { createRandomString } from "@/libs/createRandomId";
 import LoginButton from "@modules/components/button/LoginButton";
 import FlexBox from "@modules/layout/FlexBox";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 function KakaoLoginButtonWrapper() {
+  const { push } = useRouter();
+  const [clickedNumber, setClickedNumber] = useState(0);
+  const redirectUri = `${window.location.origin}/oauth/kakao`;
   const kakaoSDKLogin = () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const kakao = (window as any).Kakao;
     if (!kakao?.isInitialized()) {
       kakao?.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
     }
-    const redirectUri = `${window.location.origin}/oauth/kakao`;
     kakao?.Auth?.authorize({
       redirectUri,
       state: createRandomString(32),
     });
   };
+  const kakaoRESTLogin = () => {
+    push(
+      `https://kauth.kakao.com/oauth/authorize?redirect_uri=${redirectUri}&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_KEY}&response_type=code`,
+    );
+  };
+
   return (
     <FlexBox className="px-4 w-full justify-center">
       <div className="w-full max-w-[360px]">
-        <LoginButton type="kakao" onClick={kakaoSDKLogin} />
+        <LoginButton
+          type="kakao"
+          onClick={() => {
+            alert(clickedNumber);
+            if (clickedNumber === 0) kakaoSDKLogin();
+            else kakaoRESTLogin();
+            setClickedNumber(clickedNumber + 1);
+          }}
+        />
       </div>
     </FlexBox>
   );
