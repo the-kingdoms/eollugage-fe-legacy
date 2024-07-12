2e0a76f033e7da89029427bf1a48c7c23c608028
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-07-13 03:39:18 +0900

    FIX: 경우별 /redirect 분기

--- src/pages/oauth/redirect.tsx
+++ src/pages/oauth/redirect.tsx
@@ -2,31 +2,41 @@ import { PostRelationBody } from "@/apis/relation";
 import { myAtom } from "@/data/global";
 import { useGetInviteData } from "@/hooks/query/dynamodb";
 import { useStaffJoin } from "@/hooks/query/relation";
-import { InviteResponse } from "@/screen/manage/ShareLink";
-import axios from "axios";
 import { useAtom } from "jotai";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Redirect() {
-  const { push } = useRouter();
+  const router = useRouter();
+
   const [my] = useAtom(myAtom);
+
   const { data: inviteResponse, isSuccess } = useGetInviteData();
   const { mutate: staffJoinMutate } = useStaffJoin();
 
   useEffect(() => {
-    if (isSuccess && inviteResponse) {
-      const inviteData = inviteResponse.inviteData;
-      const body: PostRelationBody = {
-        role: "STAFF",
-        position: inviteData.position,
-      };
-      staffJoinMutate({
-        storeId: inviteData.storeId,
-        memberId: my?.id as string,
-        body,
-        inviteSchedule: inviteData.schedule,
-      });
+    const inviteId = localStorage.getItem("inviteDataId");
+    if (inviteId !== null) {
+      // 1. 직원초대링크를 통해 들어온 새 직원
+      if (isSuccess && inviteResponse) {
+        const body: PostRelationBody = {
+          role: "STAFF",
+          position: inviteResponse.inviteData.position,
+        };
+        const inviteRequestBody = {
+          storeId: inviteResponse.inviteData.storeId,
+          memberId: my?.id as string,
+          body,
+          inviteSchedule: inviteResponse.inviteData.schedule,
+        };
+        staffJoinMutate(inviteRequestBody);
+      }
+    } else if (my?.relationList.length === 0) {
+      // 2. 회원가입
+      router.push("/signup");
+    } else {
+      // 3. 기존회원 로그인
+      router.push("/main");
     }
   }, [isSuccess]);
 }
