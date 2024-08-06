e4eeaa5599b65a8a842cc067321407cfb9e3f6e3
developerryou <developerryou@gmail.com>
2024-08-06 16:05:41 +0900

    HOTFIX: api 수정에 따른 data 요청 수정

--- src/apis/store.ts
+++ src/apis/store.ts
@@ -3,6 +3,14 @@ import api, { ApiResponse } from "@/apis/network";
 
 interface PostStoreBody {
   name: string;
+  introduction: string;
+  image: string;
+  phone: string;
+  address: string;
+  openingHour: string;
+  originalInfo: string;
+  externalNotice: string;
+  internalNotice: string;
 }
 
 async function getStore(storeId: string): Promise<Store> {
